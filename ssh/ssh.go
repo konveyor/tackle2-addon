3f77a363995a7b7e47176653c1e4e622b08f4409
Jeff Ortel <jortel@redhat.com>
2025-10-22 20:33:18 -0500

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- ssh/ssh.go
+++ ssh/ssh.go
@@ -3,6 +3,7 @@ package ssh
 import (
 	"os"
 
+	"github.com/konveyor/tackle2-addon/command"
 	"github.com/konveyor/tackle2-addon/logging"
 
 	"github.com/konveyor/tackle2-hub/ssh"
@@ -11,6 +12,7 @@ import (
 func init() {
 	ssh.Log = logging.New()
 	ssh.Home, _ = os.Getwd()
+	ssh.NewCommand = command.New
 }
 
 // Agent agent.
